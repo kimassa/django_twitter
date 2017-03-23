44233c29768a5015199c4652f8bcca59fde9f2b5
kimassa <yeswemadeitfinally@gmail.com>
2017-03-24 07:48:41 +0900

    Tweet from Homepage

--- src/django_twitter/urls.py
+++ src/django_twitter/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-
-
+from tweets.views import TweetListView
 from .views import home
 
 urlpatterns = [
      url(r'^admin/', admin.site.urls), #admin/
-     url(r'^$', home, name='home'), #/
+     url(r'^$', TweetListView.as_view(), name='home'), #/
      url(r'^tweet/', include('tweets.urls', namespace='tweet')),
 
 ]
