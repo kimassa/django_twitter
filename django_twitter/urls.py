4a8b4d88d960fab58b763da7f57c5ed8f586fe61
kimassa <yeswemadeitfinally@gmail.com>
2017-03-24 04:08:01 +0900

    첫 view  완성

--- django_twitter/urls.py
+++ django_twitter/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 
@@ -7,4 +7,6 @@ from .views import home
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home, name='home'),
+    url(r'^tweet/', include('tweets.urls')),
+
 ]
