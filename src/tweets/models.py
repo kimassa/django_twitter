405c034358be1e71b2bad04f3f5b1735bdd6d20b
kimassa <yeswemadeitfinally@gmail.com>
2017-03-24 06:44:08 +0900

    부트스트랩 Containers & Columns

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
 from django.db import models
 
