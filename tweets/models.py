0f0c709dec8628536ea374dc56495887cc53bb9d
kimassa <yeswemadeitfinally@gmail.com>
2017-03-24 02:49:55 +0900

    model에 필드 변경

--- tweets/models.py
+++ tweets/models.py
@@ -1,10 +1,9 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
 
 class Tweet(models.Model):
-    content     = models.TextField()
-
+    content   = models.CharField(max_length=140)
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
     def __str__(self):
          return str(self.content)
